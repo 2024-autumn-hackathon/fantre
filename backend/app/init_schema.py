3c2c3c5d423fcb606c9d5e43184a8ba29d1ff2b5
abyMaya <abycat22@gmail.com>
2024-11-03 23:10:34 +0900

    [update]Item追加

--- backend/app/init_schema.py
+++ backend/app/init_schema.py
@@ -6,18 +6,19 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 import asyncio
 from app.database.db_connection import Database
-from app.models import User, CollectionList
+from app.models import Item, User, CollectionList
 
 # 初期設定
 async def init_schema(database):
     db = Database()
     database = await db.connect()  # データベースに接続
     
-    await init_beanie(database, document_models=[  CollectionList, User])
+    await init_beanie(database, document_models=[Item, User, CollectionList])
 
 # 初期データを挿入
-# ユーザーを取得
-    test_user = await User.find_one({"user_name": "Test User"})  # ユーザーを探す
+    # ユーザーを挿入
+    # Test Userという名前のユーザーが存在しない場合アイテムを作成
+    test_user = await User.find_one({"user_name": "Test User"}) 
     if not test_user:  # ユーザーが存在しない場合
         test_user = User(
             _id=ObjectId(),
@@ -28,3 +29,17 @@ async def init_schema(database):
         )
     await test_user.insert()  # データベースにユーザーを追加
 
+    # グッズを挿入
+    # Test Itemという名前のグッズが存在しない場合グッズを作成
+    if not await Item.find_one({"item_name": "Test Item"}):        
+        test_item = Item(
+            _id=ObjectId(),
+            item_name="Test Item",
+            item_series=ObjectId("60d5f484a2d21a1d4cf1b0e6"),
+            item_character=ObjectId("60d5f484a2d21a1d4cf1b0e7"),
+            category=ObjectId("60d5f484a2d21a1d4cf1b0e8"),
+            tags=["#test1", "#test2"],
+            jan_code="4991567672501",
+            retailers=["Test Shop"]
+        )
+        await test_item.insert()  # データベースにグッズを追加
