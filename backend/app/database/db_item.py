96cea788683f459b0510befcdc26a9f30cebcb9a
abyMaya <abycat22@gmail.com>
2024-11-09 12:48:06 +0900

    [add]item登録機能

---
+++ backend/app/database/db_item.py
@@ -0,0 +1,21 @@
+# backend/app/database/db_item.py
+from app.models import Item
+from bson import ObjectId
+from fastapi import HTTPException
+from pydantic import BaseModel
+from app.database.db_connection import Database
+
+
+db = Database()
+
+# 新しいアイテムを作成する
+async def create_item(item: Item):
+    await db.connect()
+    print(item)  # 挿入するアイテムの内容を表示
+    try:
+        await item.insert()
+        return item
+    except Exception as e:
+        print(f"Error occurred: {e}")  # エラーログを出力
+    finally:
+        await db.disconnect()  # 接続を切断
