31c012eb4741ba22e856cf7610ed6f857ede2398
abyMaya <abycat22@gmail.com>
2024-11-10 07:27:42 +0900

    [update]エラーハンドリング微修正

--- backend/app/database/db_item.py
+++ backend/app/database/db_item.py
@@ -27,6 +27,14 @@ async def get_items():
         items = await Item.find_all().to_list()
         return items
     except Exception as e:
-        print(f"Error occurred: {e}")  # エラーログを出力
+        raise Exception(f"Error fetching items: {str(e)}")  # エラーログを出力
     finally:
         await db.disconnect()  # 接続を切断
+
+# 指定したユーザーの独自データ（カスタムアイテム等すべての詳細を含む）を取得する
+async def get_user_specific_data(user_id: str):
+    try:
+        user_specific_data = await UserSpecificData.find_one({"user_id": ObjectId(user_id)})
+        return user_specific_data
+    except Exception as e:
+        raise Exception(f"Error fetching user specific data: {str(e)}")  # エラーログを出力
