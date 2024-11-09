4886bd8635c80c753c3467ec05fc0056028e6f8a
abyMaya <abycat22@gmail.com>
2024-11-09 19:04:01 +0900

    [update]item_name重複回避

--- backend/app/database/db_item.py
+++ backend/app/database/db_item.py
@@ -19,3 +19,14 @@ async def create_item(item: Item):
         print(f"Error occurred: {e}")  # エラーログを出力
     finally:
         await db.disconnect()  # 接続を切断
+
+# すべてのアイテム（詳細含む）を取得する
+async def get_items():
+    await db.connect()
+    try:
+        items = await Item.find_all().to_list()
+        return items
+    except Exception as e:
+        print(f"Error occurred: {e}")  # エラーログを出力
+    finally:
+        await db.disconnect()  # 接続を切断
