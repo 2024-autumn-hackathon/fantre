dfd27fddf9dcd82fdaf69e35ccfa6c3608c6851f
abyMaya <abycat22@gmail.com>
2024-11-10 21:48:08 +0900

    [update]Validationコメントアウト

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,12 +23,12 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-# ValidationErrorでの422エラーのレスポンス変更
-@app.exception_handler(RequestValidationError)
-async def error_handler(request: Request, exc: RequestValidationError):
-    return JSONResponse(
-        content={"detail": "There was an error in your input. Please"}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
-    )
+# # ValidationErrorでの422エラーのレスポンス変更
+# @app.exception_handler(RequestValidationError)
+# async def error_handler(request: Request, exc: RequestValidationError):
+#     return JSONResponse(
+#         content={"detail": "There was an error in your input. Please"}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+#     )
 
 # ルーター追加
 app.include_router(user_router)  # ユーザー関連のルーターを追加
