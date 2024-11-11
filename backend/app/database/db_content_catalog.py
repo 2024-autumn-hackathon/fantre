c702b96b1db20a8592bf427e1378f5c5723e3db2
abyMaya <abycat22@gmail.com>
2024-11-12 07:45:21 +0900

    [add]create_category

--- backend/app/database/db_content_catalog.py
+++ backend/app/database/db_content_catalog.py
@@ -1,6 +1,8 @@
 # backend/app/database/db_content_catalog.py
-from app.models import ContentCatalog
+from bson import ObjectId
+from app.models import Category, ContentCatalog
 from app.database.db_connection import Database
+from fastapi import HTTPException, status
 
 db = Database()
 
@@ -10,7 +12,10 @@ async def save_content_catalog(content_catalog: ContentCatalog):
     try:
         await content_catalog.save()
     except Exception as e:
-        raise Exception(f"Error saving content catalog: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error saving content catalog: {str(e)}"
+            )
     finally:
         await db.disconnect()
 
@@ -23,9 +28,37 @@ async def get_content_catalog():
             content_catalog = ContentCatalog()
         return content_catalog
     except Exception as e:
-        raise Exception(f"Error saving content catalog: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error fetching content catalog: {str(e)}"
+            )
     finally:
         await db.disconnect()
+
+# 新しいグッズジャンル(カテゴリー)を作成する 
+async def create_category(category_name: str):
+    try:
+        content_catalog = await get_content_catalog()
+        # 既存のcategory_nameとの重複を確認
+        if content_catalog:
+            for existing_category in content_catalog.categories:
+                if existing_category.category_name == category_name:
+                    raise HTTPException(
+                        status_code=status.HTTP_400_BAD_REQUEST,
+                        detail="Category with the same name already exists."
+                    )
+        new_category = Category(_id=ObjectId(), category_name=category_name)
+        content_catalog.categories.append(new_category)
+        await save_content_catalog(content_catalog)
+        return new_category
+    
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An unexpected error occurd while creating the category."
+        )
 
 # すべてのグッズジャンル（詳細を含む）を取得する
 async def get_categories():
