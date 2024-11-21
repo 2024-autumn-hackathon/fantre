3bab9698f40dbd77200e3727baabccaa994e8e9c
abyMaya <abycat22@gmail.com>
2024-11-21 17:42:31 +0900

    [update] DEBUG用print文削除

--- backend/app/api/item.py
+++ backend/app/api/item.py
@@ -350,7 +350,6 @@ async def get_filtered_items(
     except HTTPException as e:
         raise e
     except Exception as e:
-        print(f"Unexpected error when fetching the filtered_items: {str(e)}")  # 詳細なエラーをログに出力
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="An error occured while fetching the filtered_items"
@@ -365,7 +364,6 @@ class CustomItemUpdate(BaseModel):
     custom_item_tags: Optional[List[str]] = None  
     custom_item_retailers: Optional[List[str]] = None
 
-
 @router.patch("/api/items/{item_id}")
 async def update_custom_item(item_id: str, updated_data: CustomItemUpdate):
 
@@ -376,13 +374,11 @@ async def update_custom_item(item_id: str, updated_data: CustomItemUpdate):
         item = await Item.find_one({"_id": ObjectId(item_id)})
         if not item:
             raise HTTPException(status_code=404, detail="Item not found")
-        print("item", item)
         
         # ユーザーの独自データを取得
         user_specific_data = await get_user_specific_data(user_id)
         # 独自データがない場合新規作成
         if not user_specific_data:
-            print("No user_specific_data found. Creating a new one.")
 
             user_specific_data = UserSpecificData(
                 user_id=user_id,
@@ -393,13 +389,8 @@ async def update_custom_item(item_id: str, updated_data: CustomItemUpdate):
             )
             user_specific_data = await create_user_specific_data(user_id, user_specific_data)
 
-        print("user_specific_data", user_specific_data)
-
         # ユーザーの custom_series_names 内に 該当するseries_id が存在するか確認
         existing_series_names = next((s for s in user_specific_data.custom_series_names if s.series_id == item.item_series), None)
-
-        print("existing_series_names",existing_series_names)
-        print("series_id", item.item_series)
 
         if not existing_series_names:
             # series_idからseries_nameを取得する
@@ -412,25 +403,16 @@ async def update_custom_item(item_id: str, updated_data: CustomItemUpdate):
                     custom_series_name=series_name
                 )
             user_specific_data.custom_series_names.append(new_series)
-            print("New Series ", new_series)
 
         # ユーザーがシリーズ名を入力しているならデータ更新
         if updated_data.custom_series_name:
-            print("input_custom_series_name",updated_data.custom_series_name)
-
             # 再確認
             existing_series_names = next((s for s in user_specific_data.custom_series_names if s.series_id == item.item_series), None)
             # 名前更新
             existing_series_names.custom_series_name = updated_data.custom_series_name
-            
-            # await existing_series_names.save()  # 更新を保存
-            print("Updated Series Name:", existing_series_names.custom_series_name)
 
         # ユーザーの custom_character_names 内に 該当するcharacter_id が存在するか確認
         existing_character_names = next((c for c in user_specific_data.custom_character_names if c.character_id == item.item_character), None)
-
-        print("existing_character_names",existing_character_names)
-        print("character_id", item.item_character)
 
         if not existing_character_names:
             # character_idからcharacter_nameを取得する
@@ -443,25 +425,17 @@ async def update_custom_item(item_id: str, updated_data: CustomItemUpdate):
                     custom_character_name=character_name
                 )
             user_specific_data.custom_character_names.append(new_character)
-            print("New Character ", new_character)
 
         # ユーザーがシリーズ名を入力しているならデータ更新
         if updated_data.custom_character_name:
-            print("input_custom_character_name",updated_data.custom_character_name)
 
             # 再確認
             existing_character_names = next((c for c in user_specific_data.custom_character_names if c.character_id == item.item_character), None)
             # 名前更新
-            existing_character_names.custom_character_name = updated_data.custom_character_name
-            
-            # await existing_series_names.save()  # 更新を保存
-            print("Updated Character Name:", existing_character_names.custom_character_name)
+            existing_character_names.custom_character_name = updated_data.custom_character_name           
 
     # ユーザーの custom_category_names 内に 該当するcategory_id が存在するか確認
         existing_category_names = next((cat for cat in user_specific_data.custom_category_names if cat.category_id == item.category), None)
-
-        print("existing_category_names",existing_category_names)
-        print("category_id", item.category)
 
         if not existing_category_names:
             # series_idからseries_nameを取得する
@@ -474,23 +448,17 @@ async def update_custom_item(item_id: str, updated_data: CustomItemUpdate):
                     custom_category_name=category_name
                 )
             user_specific_data.custom_category_names.append(new_category)
-            print("New Category ", new_category)
 
         # ユーザーがシリーズ名を入力しているならデータ更新
         if updated_data.custom_category_name:
-            print("input_custom_category_name",updated_data.custom_category_name)
 
             # 再確認
             existing_category_names = next((cat for cat in user_specific_data.custom_category_names if cat.category_id == item.category), None)
             # 名前更新
-            existing_category_names.custom_category_name = updated_data.custom_category_name
-            
-            # await existing_series_names.save()  # 更新を保存
-            print("Updated Category Name:", existing_category_names.custom_category_name)
+            existing_category_names.custom_category_name = updated_data.custom_category_name           
 
         # カスタムアイテムがあるか確認
         custom_item = next((ci for ci in user_specific_data.custom_items if ci.item_id == ObjectId(item_id)), None)
-        print("custom_item", custom_item)
                 
         # カスタムアイテムの更新
         if custom_item:
@@ -514,15 +482,9 @@ async def update_custom_item(item_id: str, updated_data: CustomItemUpdate):
                 custom_item_tags = updated_data.custom_item_tags if updated_data.custom_item_tags is not None else item.tags or [],
                 custom_item_retailers = updated_data.custom_item_retailers if updated_data.custom_item_retailers is not None else item.retailers or []          
             )
-            print("item.tags")
-            print("updated_data.custom_item_tags", updated_data.custom_item_tags)  
-
             custom_item = await create_custom_item(user_specific_data, custom_item)
-            print("custom_item", custom_item)
         
         await user_specific_data.save()
-        print("User specific data saved:", user_specific_data)
-
         return custom_item
 
     except ValidationError as e:
@@ -533,7 +495,6 @@ async def update_custom_item(item_id: str, updated_data: CustomItemUpdate):
     except HTTPException as e:
         raise e
     except Exception as e:
-        print(f"Unexpected error when updating the custom_item: {str(e)}")  # 詳細なエラーをログに出力
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="An error occured while updating the custom_item"
