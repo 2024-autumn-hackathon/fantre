7a375c94b1d9051e5985d2911ebfebc05c8221cb
Hisanori Ito <86972518+hisanori-ito@users.noreply.github.com>
2024-11-23 14:10:43 +0900

    [fix]コレクションリスト画面調整

--- frontend/src/app/lists/[listId]/page.tsx
+++ frontend/src/app/lists/[listId]/page.tsx
@@ -1,8 +1,72 @@
+import MonitorLayout_SSR from "@/components/MonitorLayout_SSR"
+import TopButton from "@/components/TopButton"
+import Checkbox from "@/components/Checkbox"
+import PagenationListContainer from "@/components/PagenationListContainer"
+import PagenationListItem from "@/components/PagenationListItem"
+import PagenationNavi from "@/components/PagenationNavi"
+import PagenationNaviContainer from "@/components/PagenationNaviContainer"
+import TextLinkButton from "@/components/TextLinkButton"
+import OnClickButton from "@/features/common/OnClickButton"
+import Image from "next/image"
+
 const ListDetailPage = () => {
-  return (
+  const viewContent = (
     <>
-      <p>Collection List Details</p>
+      <PagenationListContainer>
+        <PagenationListItem>
+          <TextLinkButton href="/items/itemId1">
+            グッズ 1
+          </TextLinkButton>
+          <Checkbox/>
+        </PagenationListItem>
+        <PagenationListItem>
+          <TextLinkButton href="/items/itemId2">
+            グッズ 2
+          </TextLinkButton>
+          <Checkbox/>
+        </PagenationListItem>
+        <PagenationListItem>
+          <TextLinkButton href="/items/itemId3">
+            グッズ 3
+          </TextLinkButton>
+          <Checkbox/>
+        </PagenationListItem>
+      </PagenationListContainer>
+      <PagenationNaviContainer>
+        <PagenationNavi href="/lists?page=1">1</PagenationNavi>
+        <PagenationNavi href="/lists?page=2">2</PagenationNavi>
+      </PagenationNaviContainer>
+
+      {/* stateの分岐で画像表示と切り替え */}
+      <Image
+        fill
+        src="/torio.png"
+        alt="背景として表示する画像です"
+        style={{objectFit:"contain"}}
+      />
     </>
+  )
+
+  const naviContent = (
+    <div className="h-full">
+      <div>
+        <span>コレクション一覧を表示中・・・</span>
+      </div>
+      <OnClickButton addClass="h-4/5">チェック項目をリストから削除</OnClickButton>
+
+      {/* stateの分岐で文言の表示を切り替え */}
+      <OnClickButton addClass="h-4/5">画像ビューに切り替え</OnClickButton>
+      <OnClickButton addClass="h-4/5">テキストビューに切り替え</OnClickButton>
+    </div>
+  ) 
+
+  return (
+    <MonitorLayout_SSR
+      headerContent={ <TopButton/> }
+      viewContent={ viewContent }
+      naviContent={ naviContent }
+      footerContent
+    />
   )
 }
 
--- frontend/src/app/lists/page.tsx
+++ frontend/src/app/lists/page.tsx
@@ -1,8 +1,68 @@
+import InputButton from "@/components/InputButton"
+import MonitorLayout_SSR from "@/components/MonitorLayout_SSR"
+import TopButton from "@/components/TopButton"
+import Checkbox from "@/components/Checkbox"
+import PagenationListContainer from "@/components/PagenationListContainer"
+import PagenationListItem from "@/components/PagenationListItem"
+import PagenationNavi from "@/components/PagenationNavi"
+import PagenationNaviContainer from "@/components/PagenationNaviContainer"
+import TextLinkButton from "@/components/TextLinkButton"
+import OnClickButton from "@/features/common/OnClickButton"
+
+
 const ListsPage = () => {
-  return (
+
+  const viewContent = (
     <>
-      <p>Collection Lists</p>
+      <PagenationListContainer>
+        <PagenationListItem>
+          <TextLinkButton href="/lists/1">
+            list 1
+          </TextLinkButton>
+        </PagenationListItem>
+        <PagenationListItem>
+          <TextLinkButton href="/lists/2">
+            list 2
+          </TextLinkButton>
+        </PagenationListItem>
+        <PagenationListItem>
+          <TextLinkButton href="/lists/3">
+            list 3
+          </TextLinkButton>
+        </PagenationListItem>
+        <PagenationListItem>
+          <TextLinkButton href="/items/itemId1">
+            グッズ3だよ
+          </TextLinkButton>
+          <Checkbox/>
+          <OnClickButton addClass="h-4/5">画像を表示</OnClickButton>
+        </PagenationListItem>
+      </PagenationListContainer>
+      <PagenationNaviContainer>
+        <PagenationNavi href="/lists?page=1">1</PagenationNavi>
+        <PagenationNavi href="/lists?page=2">2</PagenationNavi>
+      </PagenationNaviContainer>
     </>
+  )
+
+  const naviContent = (
+    <div className="h-full">
+      <div>
+        <span>コレクションリスト一覧を表示中・・・</span>
+      </div>
+      <InputButton defaultValue="グッズを探す"/>
+      <InputButton defaultValue="コレクションリスト名"/>
+      <InputButton defaultValue="コレクションリストを作成"/>
+    </div>
+  ) 
+
+  return (
+    <MonitorLayout_SSR
+      headerContent={ <TopButton/> }
+      viewContent={ viewContent }
+      naviContent={ naviContent }
+      footerContent
+    />
   )
 }
 
