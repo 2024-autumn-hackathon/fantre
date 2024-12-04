5216eef2caf2f317cac6d0ed2aded3d22c930a71
ゆういち <yuichi-star-alchemist@users.noreply.github.com>
2024-12-04 13:34:00 +0900

    [add] characterページのデータ取得実装

--- frontend/src/app/series/[seriesId]/page.tsx
+++ frontend/src/app/series/[seriesId]/page.tsx
@@ -1,62 +1,26 @@
-"use client"
+import CharactersPage from "@/features/routes/series/characters/CharactersPage"
+import { getRequestByPage } from "@/utils/getRequestFromServer"
 
-import MonitorLayout from "@/components/MonitorLayout"
-import TextLinkButton from "@/components/TextLinkButton"
-import TextViewButton from "@/components/TextViewButton"
-import TopButton from "@/components/TopButton"
-import PagenationListContainer from "@/features/common/pagenation/components/PagenationListContainer"
-import PagenationListItem from "@/features/common/pagenation/components/PagenationListItem"
-import PagenationNaviContainer from "@/features/common/pagenation/components/PagenationNaviContainer"
-import PageState from "@/features/common/pagenation/PageState"
-import { useState } from "react"
-
-// const CharactersPage = ({
-const CharactersPage = async (
-  context: {
-    params: Promise<{
-      seriesId: string
-    }>
-  }
-) => {
-  const [pageState, setPageState] = useState<PageState>({currentPage: 1, maxPage: 1})
-  const { seriesId } = await context.params
-  const viewContent = (
-    <>
-      <PagenationListContainer>
-        <PagenationListItem>
-          <TextLinkButton href={`/items?seriesId=${ seriesId }&characterId=characterId1`}>
-            キャラ1だよ
-          </TextLinkButton>
-        </PagenationListItem>
-        <PagenationListItem>
-          <TextLinkButton href={`/items?seriesId=${ seriesId }&characterId=characterId2`}>
-            キャラ2だよ
-          </TextLinkButton>
-        </PagenationListItem>
-        <PagenationListItem>
-          <TextLinkButton href={`/items?seriesId=${ seriesId }&characterId=characterId3`}>
-            キャラ3だよ
-          </TextLinkButton>
-        </PagenationListItem>
-      </PagenationListContainer>
-      <PagenationNaviContainer
-        pageState={ pageState }
-        handleSetPageState={ setPageState }
-      />
-    </>
-  )
-  return (
-    <MonitorLayout
-      headerContent={ <TopButton/> }
-      viewContent={ viewContent }
-      naviContent={
-        <div className="flex h-full items-center">
-          <TextViewButton>キャラ一覧を表示中・・・</TextViewButton>
-        </div>
-      }
-      footerContent
-    />
-  )
+const initialFetchedCharactersPage = async ({
+  params,
+  searchParams,
+}: Readonly<{
+  params: Promise<{ seriesId: string}>
+  searchParams: Promise<{ seriesName: string }>
+}>) => {
+  const seriesId = (await params).seriesId
+  const seriesName = (await searchParams).seriesName
+  const response = await getRequestByPage(`series/${ seriesId }/characters`)
+  if (!response) return
+  const charactersList = response.characters
+  const maxPage = response.all_pages
+  
+  return <CharactersPage
+    initialCharactersList={ charactersList }
+    maxPage={ maxPage }
+    seriesId={ seriesId }
+    seriesName={  seriesName}
+  />
 }
 
-export default CharactersPage
+export default initialFetchedCharactersPage
